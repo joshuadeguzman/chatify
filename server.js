e8fa22dd4226b74208b93233149e4f7c97c5f01f
Xeleb iOS Developer <ojt.joshua.deguzman@xurpas.com>
2018-03-02 16:37:57 +0800

    Update description. Add .idea/ on git ignore

--- server.js
+++ server.js
@@ -10,23 +10,31 @@ app.get('/', function (req, res) {
 io.on('connection', function (socket) {
 
     socket.on('add-user', function (username) {
+        // Assign session username
         socket.username = username;
+
+        // Add user to pool of users
         usernames[username] = username;
 
+        // Update chat room with 2 param message
         socket.emit('update-chat', 'SERVER', username +' you have joined the chatify room');
 
+        // Globally broadcast user who joined the chat room
         socket.broadcast.emit('update-chat', 'SERVER', username +' has connected to the chatify room');
 
+        // Update pool of users display
         io.sockets.emit('update-users', usernames);
 
+        // Log
         console.log(username + ' joined the chatify room');
 
     });
 
+    // Send message
     socket.on('send-message', function (msg) {
         io.sockets.emit('update-chat', socket.username,msg);
     });
-    
+
 });
 
 http.listen(8000, function () {
