caaa9b4eeab79690916e93402e60ed93aca307e0
Xeleb iOS Developer <ojt.joshua.deguzman@xurpas.com>
2018-03-02 16:35:46 +0800

    Add nicknames for unique socket

--- server.js
+++ server.js
@@ -1,23 +1,34 @@
-
-
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var usernames = {};
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-  console.log('a user connected');
+io.on('connection', function (socket) {
+
+    socket.on('add-user', function (username) {
+        socket.username = username;
+        usernames[username] = username;
+
+        socket.emit('update-chat', 'SERVER', username +' you have joined the chatify room');
+
+        socket.broadcast.emit('update-chat', 'SERVER', username +' has connected to the chatify room');
+
+        io.sockets.emit('update-users', usernames);
+
+        console.log(username + ' joined the chatify room');
+
+    });
+
+    socket.on('send-message', function (msg) {
+        io.sockets.emit('update-chat', socket.username,msg);
+    });
+    
 });
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
-});
-
-http.listen(8000, function(){
-  console.log('listening on *:8000');
+http.listen(8000, function () {
+    console.log('listening on *:8000');
 });
