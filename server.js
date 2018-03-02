336d9fdd184a42e05b921fec73f64a30bb469724
Xeleb iOS Developer <ojt.joshua.deguzman@xurpas.com>
2018-03-02 16:13:08 +0800

    Update port definition

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var port = 8000;
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -18,6 +19,6 @@ io.on('connection', function(socket){
   });
 });
 
-http.listen(8000, function(){
-  console.log('listening on *:8000');
+http.listen(port, function(){
+  console.log('listening on  ' + port);
 });
